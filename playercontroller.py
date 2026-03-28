044be731a1798f78ce14d8895720ea95d44764cf
SamuelSaylor <samuelsaylor2025@gmail.com>
2026-03-28 12:43:31 -0500

    Introdced playercontroller.py

---
+++ playercontroller.py
@@ -0,0 +1,24 @@
+def accelerate():
+    pass
+
+def braking():
+    pass
+
+def turning(BUTTON):
+    pass
+
+
+
+'''
+keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_UP]:
+        input_accel = 1
+    elif keys[pygame.K_DOWN]:
+        input_accel = -1
+
+    if keys[pygame.K_LEFT]:
+        input_dir = -1
+    elif keys[pygame.K_RIGHT]:
+        input_dir = 1
+'''
